4164a360f9c421850b60dfc8e2bad88a1adb0164
Ian Hundere <ianfundere@gmail.com>
2018-10-11 09:37:19 -0400

    adds scripts

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+console.log(`oi boi`);
+// Classes are for CSS stlying
+// "data attributes" are for JS
+
+
+
+// #1 code will run when button is clicked
+const buttonElement = document.querySelector()
+
+
+// #2 write text to the page on button click
+
+// #3 pull text from an array of text
+// to write to the page
